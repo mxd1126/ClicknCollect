975b033cba03ca5b3b8330867e07b820f90ee0cc
Manish Dhingra <mdhingra@sapient.com>
2017-05-05 16:07:44 -0400

    updated the email id.

--- ClicknCollect.rb
+++ ClicknCollect.rb
@@ -6,7 +6,8 @@ require 'watir'
 browser=Watir::Browser.new :firefox
 browser.cookies.clear
 browser.goto'https://www.loblaws.ca/login'
-browser.text_field(:id =>'email_login').set'dhingrmanish26@gmail.com'
-browser.text_field(:id =>'password_login').set'Canada@01'
+browser.text_field(:id =>'email_login').set'exactmytarget+099@gmail.com'
+browser.text_field(:id =>'password_login').set'Canada01'
+browser.wait 50
 browser.button(xpath: '//form[@id="loginForm_login"]//button[contains(@class, "input-signin")]').click
 

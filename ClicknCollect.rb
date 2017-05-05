5689977e6a397aefd90b5a86ea612d153a2ec056
Manish Dhingra <mdhingra@sapient.com>
2017-05-05 15:48:12 -0400

    changes for login

--- ClicknCollect.rb
+++ ClicknCollect.rb
@@ -3,6 +3,10 @@ require 'watir'
 
 
 
-browser=Watir::Browser.new:firefox
+browser=Watir::Browser.new :firefox
+browser.cookies.clear
 browser.goto'https://www.loblaws.ca/login'
 browser.text_field(:id =>'email_login').set'dhingrmanish26@gmail.com'
+browser.text_field(:id =>'password_login').set'Canada@01'
+browser.button(xpath: '//form[@id="loginForm_login"]//button[contains(@class, "input-signin")]').click
+
